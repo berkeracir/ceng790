79837ab83e1c451a905932f5eb3857b30a0876c0
berkeracir159@gmail.com <159.Berker>
2020-11-11 22:02:25 +0300

    finalize Part 2

--- assignment1/src/edu/metu/ceng790/hw1/Part2.scala
+++ assignment1/src/edu/metu/ceng790/hw1/Part2.scala
@@ -15,8 +15,8 @@ object Part2 {
       
       // YOUR CODE HERE
       // 1. Display the 5 lines of the RDD (take(5)) and display the number of elements in the RDD (count()).
-      println(originalFlickrMeta.count())
       originalFlickrMeta.take(5).foreach(println)
+      println(originalFlickrMeta.count())
 
       // 2. Transform the RDD[String] in RDD[Picture] using the Picture class. Only keep interesting pictures having a
       // valid country and tags. To check your program, display 5 elements.
@@ -27,6 +27,7 @@ object Part2 {
       // 3. Now group these images by country (groupBy). Print the list of images corresponding to the first country.
       // What is the type of this RDD?
       val picturesByCountries = pictures.groupBy(picture => picture.c)
+      // Countries and their pictures count
       picturesByCountries.map(x => (x._1, x._2.size)).foreach(x => printf("%s(%d)\n", x._1, x._2))
 
       // 4. We now wish to process an RDD containing pairs in which the first element is a country, and the second
@@ -41,7 +42,7 @@ object Part2 {
       // function, equivalent to groupBy(x=>x) could be useful.
       val tagsFrequencyByCountries = tagsByCountries.mapValues(tags => tags.groupBy(tag => tag))
         .mapValues(pair => pair.map(p => (p._1, p._2.size)))
-      tagsFrequencyByCountries.take(1).foreach(println)
+      tagsFrequencyByCountries.foreach(println)
 
       // 6. There are often several ways to obtain a result. The method we used to compute the frequency of tags in each
       // country quickly reaches a state in which the size of the RDD is the number of countries. This can limit the
