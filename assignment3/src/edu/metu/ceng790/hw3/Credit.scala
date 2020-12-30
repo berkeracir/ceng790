81276e410b70961e58cd2f22dd685d8f2822cd8f
berkeracir159@gmail.com <159.Berker>
2020-12-30 03:06:21 +0300

    implement Task 2-6

--- assignment3/src/edu/metu/ceng790/hw3/Credit.scala
+++ assignment3/src/edu/metu/ceng790/hw3/Credit.scala
@@ -2,15 +2,15 @@ package edu.metu.ceng790.hw3
 
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
-
 import org.apache.spark.sql.SQLContext
-
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql._
-
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit, TrainValidationSplitModel}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 
 
 object Credit {
@@ -49,21 +49,113 @@ object Credit {
       .toDF("creditability", "balance", "duration", "history", "purpose", "amount", "savings", "employment",
         "instPercent", "sexMarried", "guarantors", "residenceDuration", "assets", "age", "concCredit", "apartment",
         "credits", "occupation", "dependents", "hasPhone", "foreign")
+      .cache()
 
+    // EXTRACT FEATURES
     // 1. Use a VectorAssembler to transform and return a new dataframe with all of the feature columns in a vector
     // column.
-    val labelColName = "creditability"
-    val featureColNames = creditDF.columns.filter(colName => !colName.equals(labelColName))
-    val featureColName = "features"
+    val featureColumnNames = creditDF.columns.filter(colName => !colName.equals("creditability"))
+    val featureColumnName = "features"
+    val labelColumnName = "label"
 
-    val creditDFWithFeatureVector = new VectorAssembler()
-      .setInputCols(featureColNames)
-      .setOutputCol(featureColName)
-      .transform(creditDF).cache()
-
-    val featureVectorDF = creditDFWithFeatureVector.select(featureColName).cache()
+    val featuresAssembler = new VectorAssembler()
+      .setInputCols(featureColumnNames)
+      .setOutputCol(featureColumnName)
 
     // 2. Use a StringIndexer to return a Dataframe with the creditability column added as a label
+    val labelIndexer = new StringIndexer()
+      .setInputCol("creditability")
+      .setOutputCol(labelColumnName)
+      .fit(creditDF)
+
+
+    // 3. Use randomSplit function to split the data into two sets: 75% of the data is used to train (and tune) the
+    // model, 25% will be used for testing.
+    val Array(trainCreditDF, testCreditDF) = creditDF
+      .randomSplit(Array(0.75, 0.25), seed = 4321)
+
+    // TRAIN THE MODEL AND OPTIMIZE HYPERPARAMETERS
+    // 4. Next, we train a RandomForest Classifier with the parameters:
+    //    a. maxDepth: Maximum depth of a tree. Increasing the depth makes the model more powerful, but deep trees take
+    //    longer to train.
+    //    b. maxBins: Maximum number of bins used for discretizing continuous features and for choosing how to split on
+    //    features at each node.
+    //    c. impurity: Criterion used for information gain calculation
+    //    d. auto: Automatically select the number of features to consider for splits at each tree node
+    //    e. seed: Use a random seed number, allowing to repeat the results. Use the random seed 4321 in this
+    //    assignment.
+    // The model is trained by making associations between the input features and the labeled output associated with
+    // those features. In order to find the best model, we search for the optimal combinations of the classifier
+    // parameters.
+    // You will optimize the model using a pipeline. A pipeline provides a simple way to try out different combinations
+    // of parameters, using a process called grid search, where you set up the parameters to test, and MLLib will test
+    // all the combinations. Pipelines make it easy to tune an entire model building workflow at once, rather than
+    // tuning each element in the Pipeline separately.
+    val randomForestClassifier = new RandomForestClassifier()
+      .setFeaturesCol(featureColumnName)
+      .setLabelCol(labelColumnName)
+      .setSeed(4321)
+      .setMaxMemoryInMB(8*1024)
+//    println("EXPLAIN RANDOM FOREST CLASSIFIER")
+//    println(randomForestClassifier.explainParams()) // TODO REPORT
+//    println()
+
+    // Use the ParamGridBuilder utility to construct the parameter grid with the following values: maxBins [24, 28, 32],
+    // maxDepth, [3, 5, 7], impurity [“entropy", "gini"]
+    val paramGridBuilder = new ParamGridBuilder()
+      .addGrid(randomForestClassifier.featureSubsetStrategy, Array("auto")  ++ Array("all", "onethird", "sqrt", "log2"))
+      .addGrid(randomForestClassifier.impurity, Array("entropy", "gini"))
+      .addGrid(randomForestClassifier.maxBins, Array(24, 28, 32) ++ Array(34, 36, 38, 40))
+      .addGrid(randomForestClassifier.maxDepth, Array(3, 5, 7) ++ Array(2, 4, 6, 8, 9, 10))
+      .addGrid(randomForestClassifier.numTrees, Array(4, 8, 12, 16, 20, 24, 28, 32))
+      .addGrid(randomForestClassifier.subsamplingRate, Array(0.1, 0.25, 0.5, 0.75, 1.0))
+      .build()
+
+    // 5. Next, you will create and set up a pipeline to make things easier. A Pipeline consists of a sequence of
+    // stages, each of which is either an Estimator or a Transformer.
+    // Use TrainValidationSplit that creates a (training, test) dataset pair. It splits the dataset into these two parts
+    // using the trainRatio parameter. For example, with trainRatio=0.75, TrainValidationSplit will generate a training
+    // and test dataset pair where 75% of the data is used for training and 25% for validation. Use these values in your
+    // code. Note that, this is different from the original random data split. Here, we further divide the training
+    // dataset into training and validation set, for tuning purposes. The final model that has been tuned will be used
+    // to evaluate the result on the test set.
+    val binaryClassificationEvaluator = new BinaryClassificationEvaluator()
+      .setLabelCol(labelColumnName)
+//    println("EXPLAIN BINARY CLASSIFICATION EVALUATOR")
+//    println(binaryClassificationEvaluator.explainParams()) // TODO REPORT
+//    println()
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("predictedLabel")
+      .setLabels(labelIndexer.labels)
+
+    // The TrainValidationSplit uses an Estimator, a set of ParamMaps, and an Evaluator. Estimator should be your random
+    // forest model, the ParamMaps is the parameter grid that you built in the previous step. The Evaluator should be
+    // new BinaryClassificationEvaluator().
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(randomForestClassifier)
+      .setEstimatorParamMaps(paramGridBuilder)
+      .setEvaluator(binaryClassificationEvaluator)
+      .setTrainRatio(0.75)
+      .setSeed(4321)
+//    println("EXPLAIN TRAIN VALIDATION SPLIT")
+//    println(trainValidationSplit.explainParams()) // TODO REPORT
+
+    val pipeline = new Pipeline()
+      .setStages(Array(featuresAssembler, labelIndexer, trainValidationSplit, labelConverter))
+
+    val model = pipeline.fit(trainCreditDF)
+    val bestModel = model.stages(2).asInstanceOf[TrainValidationSplitModel].bestModel
+    println(bestModel.explainParams())
+
+    // 6. Finally, evaluate the pipeline best-fitted model by comparing test predictions with test labels. You can use
+    // transform function to get the predictions for test dataset. You can use evaluator’s evaluate function to get the
+    // metrics.
+    val predictions = model.transform(testCreditDF)
+
+    val evaluateResult = binaryClassificationEvaluator.evaluate(predictions)
+    println(s"Evaluation: ${binaryClassificationEvaluator.evaluate(model.transform(trainCreditDF))} - ${binaryClassificationEvaluator.evaluate(model.transform(testCreditDF))}")
+
   }
 }
 
